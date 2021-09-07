01a9cb89a54423664811c8465989ddce5fdb7ca0
Maxim Sheshukow <maxim.sheshukow@dptechnology.com>
2021-09-07 17:56:59 +0200

    Some todo

--- tests/TestsMain.cpp
+++ tests/TestsMain.cpp
@@ -1,2 +1,11 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+
+// TODO:
+// Tag has reference.
+// TAGS -> DEPS
+// interface -> abstract
+// object -> concrete
+// Type & t -> Type& t
+// Get rid of IMplType
+// (TYPE*)nullptr -> std::declval<TYPE>()
